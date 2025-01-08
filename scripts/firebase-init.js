d71c89138fe0db7117d59c213f8e07210d3e643e
jypark8084 <134194444+jypark8084@users.noreply.github.com>
2025-01-09 08:45:32 +0900

    .

--- scripts/firebase-init.js
+++ scripts/firebase-init.js
@@ -1,22 +1,18 @@
-// firebase-init.js
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAG77bT0YaKUX33IrSjV3cc4N3nKdC_dY4",
     authDomain: "testlogin-8f196.firebaseapp.com",
     projectId: "testlogin-8f196",
-    storageBucket: "testlogin-8f196.firebasestorage.app",
+    storageBucket: "testlogin-8f196.appspot.com",
     messagingSenderId: "844746942507",
     appId: "1:844746942507:web:2d15058ffd6f692243626e",
     measurementId: "G-20WL1R0QYG"
 };
 
-
+// Firebase 앱 초기화
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-// Firebase 초기화
+export { app, db };
